b96593e39390fc1ecfe4ee85314e82950e2320cf
Sean Lee <leejoneshane@gmail.com>
2017-11-20 17:51:18 -0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -29,14 +29,15 @@ if [[ "${SAMBA_DOMAIN}" != "tld" && "${SAMBA_DNS_REALM}" != "tld.your.domain" ]]
         --dns-backend="SAMBA_INTERNAL"
     echo "${SAMBA_DOMAIN} - Domain ${SAMBA_DC_ACT} Successfully."
   fi
+  
   sed -ri \
       -e "s/(search) .*/\1 ${SAMBA_DNS_REALM}/" \
       -e "s/(nameserver) .*/\1 127.0.0.1/" \
       /etc/resolv.conf
-fi
-
-if [ "$1" = 'samba' ]; then
+      
+  if [ "$1" = 'samba' ]; then
     exec /usr/sbin/samba -i
+  fi
 fi
 
 exec "$@"
