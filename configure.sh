049d085fb5807e26ce7b79f8d67bac8182027052
Sean Lee <leejoneshane@gmail.com>
2017-11-20 02:19:05 -0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -10,7 +10,14 @@ fi
 
 # Configure the AD DC
 if [[ "${SAMBA_DOMAIN}" != "tld" && "${SAMBA_DNS_REALM}" != "tld.your.domain" ]]; then
-  if[ ! -f /etc/samba/smb.conf && ]; then
+  if[ -f /etc/samba/smb.conf && ]; then
+    sed -ri \
+        -e "s/SAMBA_DOMAIN/${SAMBA_DOMAIN}/" \
+        -e "s/SAMBA_DNS_REALM/${SAMBA_DNS_REALM}/" \
+        -e "s/SAMBA_HOST/${SAMBA_HOST}/" \
+        -e "s/SAMBA_DNS_FORWARD/${SAMBA_DNS_FORWARD}/" \
+        /etc/samba/smb.conf  
+  else
     mkdir -p /samba/etc /samba/lib /samba/log
     echo "${SAMBA_DOMAIN} - Begin Domain ${SAMBA_DC_ACT}..."
     samba-tool domain ${SAMBA_DC_ACT} \
@@ -21,15 +28,11 @@ if [[ "${SAMBA_DOMAIN}" != "tld" && "${SAMBA_DNS_REALM}" != "tld.your.domain" ]]
         --realm="${SAMBA_DNS_REALM}" \
         --dns-backend="SAMBA_INTERNAL"
     echo "${SAMBA_DOMAIN} - Domain ${SAMBA_DC_ACT} Successfully."
-    
-    echo "dns forwarder = ${SAMBA_DNS_FORWARDER}" >> /samba/etc/smb.conf
-    echo "unix password sync = no" >> /samba/etc/smb.conf
-    
-    sed -ri \
-        -e "s/(search) .*/\1 ${SAMBA_DNS_REALM}/" \
-        -e "s/(nameserver) .*/\1 127.0.0.1/" \
-        /etc/resolv.conf
   fi
+  sed -ri \
+      -e "s/(search) .*/\1 ${SAMBA_DNS_REALM}/" \
+      -e "s/(nameserver) .*/\1 127.0.0.1/" \
+      /etc/resolv.conf
 fi
 
 if [ "$1" = 'samba' ]; then
