8e81b4f7274c6ab9c2373a65a6483de31e84d4bd
Sean Lee <leejoneshane@gmail.com>
2017-11-20 18:00:21 -0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -10,15 +10,15 @@ fi
 
 # Configure the AD DC
 if [[ "${SAMBA_DOMAIN}" != "tld" && "${SAMBA_DNS_REALM}" != "tld.your.domain" ]]; then
-  if[ -f /etc/samba/smb.conf && ]; then
+  if[ ! -f /etc/samba/smb.conf ]; then
+    cp /smb.conf /samba/etc/smb.conf
     sed -ri \
         -e "s/SAMBA_DOMAIN/${SAMBA_DOMAIN}/" \
         -e "s/SAMBA_DNS_REALM/${SAMBA_DNS_REALM}/" \
         -e "s/SAMBA_HOST/${SAMBA_HOST}/" \
         -e "s/SAMBA_DNS_FORWARD/${SAMBA_DNS_FORWARD}/" \
         /etc/samba/smb.conf  
-  else
-    mkdir -p /samba/etc /samba/lib /samba/log
+
     echo "${SAMBA_DOMAIN} - Begin Domain ${SAMBA_DC_ACT}..."
     samba-tool domain ${SAMBA_DC_ACT} \
         --use-rfc2307 \
