c769343a4862c87370ac0aa6c02a41a40359c10c
Sean Lee <leejoneshane@gmail.com>
2017-11-20 23:09:37 -0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,10 +3,6 @@ set -e
 
 # Configure the AD DC
 if [[ "$SAMBA_DOMAIN" != "tld" && "$SAMBA_DNS_REALM" != "tld.your.domain" ]]; then
-  sed -ri \
-      -e "s/(search) .*/\1 $SAMBA_DNS_REALM/" \
-      -e "s/(nameserver) .*/\1 127.0.0.1/" \
-      /etc/resolv.conf
   if [ ! -f /etc/samba/smb.conf ]; then
     echo "$SAMBA_DOMAIN - Begin Domain $SAMBA_DC_ACT..."
     samba-tool domain $SAMBA_DC_ACT \
@@ -17,10 +13,10 @@ if [[ "$SAMBA_DOMAIN" != "tld" && "$SAMBA_DNS_REALM" != "tld.your.domain" ]]; th
         --realm="$SAMBA_DNS_REALM" \
         --dns-backend="SAMBA_INTERNAL"
     echo "$SAMBA_DOMAIN - Domain $SAMBA_DC_ACT Successfully."
-  fi
-      
-  exec /usr/sbin/samba -i
-  exec ntpd -d
-else
-  exec bash
+    
+    ln -sf /var/lib/samba/private/krb5.conf /etc/krb5.conf
+  fi      
 fi
+
+exec /usr/sbin/samba -i
+exec ntpd -d
